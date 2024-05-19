5b7b5d45fa5979046d70842468d598d693217fdd
robin-england <robin.england94@googlemail.com>
2024-05-19 18:19:44 -0400

    Create EV class extending Car with additional charge property.

---
+++ cc_4.js
@@ -0,0 +1,26 @@
+// From previous challenge:
+// Car Class Constructor:
+class Car{
+    constructor(make,speed){
+        this.make=make
+        this.speed=speed
+    }       
+    // Accelerate Method:
+    accelerate(){
+        this.speed = (this.speed + 10)  // Adding 10 km/h
+        console.log("We are accelerating! We are now driving at " + this.speed + "km/h.")
+    }
+    // Brake Method:
+    brake(){
+        this.speed = (this.speed - 5)  // Subtracting 5 km/h
+        console.log("We are slowing down! We are now driving at " + this.speed + "km/h.")
+    }
+}
+// New code:
+// 1. Implement EV Class:
+class EV extends Car{
+    constructor(make,speed,charge){
+        super(make,speed)
+        this.charge=charge
+    }     
+}
