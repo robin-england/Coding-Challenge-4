f7d2a338a1fe18c15a64ab6ed08d003218deaa58
robin-england <robin.england94@googlemail.com>
2024-05-19 19:05:28 -0400

    Test EV class methods with different operations.

--- cc_4.js
+++ cc_4.js
@@ -1,3 +1,4 @@
+// U63166005
 // From previous challenge:
 // Car Class Constructor:
 class Car{
@@ -7,12 +8,12 @@ class Car{
     }       
     // Accelerate Method:
     accelerate(){
-        this.speed = (this.speed + 10)  // Adding 10 km/h to speed.
+        this.speed = (this.speed + 10)  // Adding 10 km/h to speed
         console.log("We are accelerating! We are now driving at " + this.speed + "km/h.")
     }
     // Brake Method:
     brake(){
-        this.speed = (this.speed - 5)  // Subtracting 5 km/h from speed.
+        this.speed = (this.speed - 5)  // Subtracting 5 km/h from speed
         console.log("We are slowing down! We are now driving at " + this.speed + "km/h.")
     }
 }
@@ -30,8 +31,19 @@ class EV extends Car{
 }
 // 3. Customize Accelerate Method:
     accelerate(){
-        this.speed = (this.speed + 20)  // Adding 20 km/h to speed.
-        this.charge = (this.charge - 1) // Decreasing charge by 1%.
+        this.speed = (this.speed + 20)  // Adding 20 km/h to speed
+        this.charge = (this.charge - 1) // Decreasing charge by 1%
         console.log("We are accelerating! We are now driving at " + this.speed + "km/h. Charge is at " + this.charge + "%.")
     }
 }
+// 4. Testing EV Class:
+let Tesla = new EV ("Tesla",120,23) // Created test car
+
+Tesla.accelerate() // Testing each function
+Tesla.brake()
+Tesla.chargeBattery(2)
+Tesla.accelerate()
+Tesla.accelerate() // This will make the battery 0%
+Tesla.accelerate() // Should not be able to accelerate anymore.
+
+// Testing shows that the charge ends at -1% and the car continues to accelerate even though it shouldn't be able to.
